226c3c5a5ac10ed8d13988c8e5eccc5396bfac03
SophieRF <sofiaferraro2110@gmail.com>
2025-06-02 18:59:54 -0300

    adelantos en detalleScreen, modificaciones en variables y types, archivos de redux,  entre otros

--- src/components/screen/LoginScreen.tsx
+++ src/components/screen/LoginScreen.tsx
@@ -3,14 +3,12 @@ import { LoginForm } from "../forms/LoginForm";
 
 
 export const LoginScreen = () => {
-  return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <main className="flex-grow flex items-center justify-center bg-[#f0f4f9] px-4">
         <div className="max-w-md w-full bg-white rounded-lg shadow-xl overflow-hidden">
           <div className="bg-[#27548a] px-6 py-4">
             <h2 className="text-center text-2xl font-bold text-white">
              Log In
-            </h2>
           </div>
 
           <div className="px-6 py-4">
@@ -22,7 +20,6 @@ export const LoginScreen = () => {
                 <Link
                   to="/registro"
                   className="font-medium text-[#27548a] hover:text-[#1e3e66] transition-colors"
-                >
                   Registrate
                 </Link>
               </p>
@@ -33,6 +30,3 @@ export const LoginScreen = () => {
 
 
 
-    </div>
-  );
-};
