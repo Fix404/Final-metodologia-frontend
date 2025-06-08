32349517ab01bc930f7524b88c22d739bde2e60a
Maximiliano Niscola <niscolamaximiliano@gmail.com>
2025-06-08 19:50:11 -0300

    más cambios en las tablas de admin, sacando cosas del subnavbar

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -10,14 +10,16 @@ import DetalleScreen from './components/screen/DetalleScreen';
 import { LoginScreen } from './components/screen/LoginScreen';
 import { CarritoScreen } from './components/screen/CarritoScreen';
 import ClienteNavbar from './components/ui/Navbar/ClienteNavbar';
-import { TablaAdminClientes } from './components/ui/TablasAdmin/TablaAdminClientes';
-import { TablaAdminEmpleados } from './components/ui/TablasAdmin/TablaAdminEmpleados';
+import { TablaAdminClientes } from './components/ui/TablasAdmin/Usuarios/TablaAdminClientes';
+import { TablaAdminEmpleados } from './components/ui/TablasAdmin/Usuarios/TablaAdminEmpleados';
+import { TablaAdminCatalogo } from './components/ui/TablasAdmin/Productos/TablaAdminCatalogo';
+import { TablaAdminOrdCompra } from './components/ui/TablasAdmin/Pedidos/TablaAdminOrdCompra';
+import { TablaAdminHistorial } from './components/ui/TablasAdmin/Pedidos/TablaAdminHistorial';
 
 export const AppRouter = () => {
     return (
         <Router>
             <Routes>
-
                 {/*Landing Page sin Footer*/}
                 <Route path="/" element={
                     <div className="flex flex-col min-h-screen">
@@ -99,6 +101,96 @@ export const AppRouter = () => {
 
               <div className="flex-grow flex items-start justify-center px-4 py-15 z-6 relative">
                 <TablaAdminClientes />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/admin/productos/catalogo"
+          element={
+            <div
+              className="flex flex-col min-h-screen bg-[url('/logoDesaturado.png')] bg-no-repeat bg-center"
+              style={{ backgroundSize: "385px auto",
+                backgroundPosition: 'center 140px'
+               }}
+            >
+              <div className="w-full bg-[#183B4E]">
+                <AdminNavBar />
+              </div>
+
+              <div>
+                <AdminSubNavBar />
+              </div>
+
+              <div className="flex-grow flex items-start justify-center px-4 py-15 z-6 relative">
+                <TablaAdminCatalogo />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/admin/productos/precios"
+          element={
+            <div
+              className="flex flex-col min-h-screen bg-[url('/logoDesaturado.png')] bg-no-repeat bg-center"
+              style={{ backgroundSize: "385px auto",
+                backgroundPosition: 'center 140px'
+               }}
+            >
+              <div className="w-full bg-[#183B4E]">
+                <AdminNavBar />
+              </div>
+
+              <div>
+                <AdminSubNavBar />
+              </div>
+
+              <div className="flex-grow flex items-start justify-center px-4 py-15 z-6 relative">
+                <TablaAdminCatalogo />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/admin/pedidos/ordenesCompra"
+          element={
+            <div
+              className="flex flex-col min-h-screen bg-[url('/logoDesaturado.png')] bg-no-repeat bg-center"
+              style={{ backgroundSize: "385px auto",
+                backgroundPosition: 'center 140px'
+               }}
+            >
+              <div className="w-full bg-[#183B4E]">
+                <AdminNavBar />
+              </div>
+              <div>
+                <AdminSubNavBar />
+              </div>
+              <div className="flex-grow flex items-start justify-center px-4 py-15 z-6 relative">
+                <TablaAdminOrdCompra />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/admin/pedidos/historial"
+          element={
+            <div
+              className="flex flex-col min-h-screen bg-[url('/logoDesaturado.png')] bg-no-repeat bg-center"
+              style={{ backgroundSize: "385px auto",
+                backgroundPosition: 'center 140px'
+               }}
+            >
+              <div className="w-full bg-[#183B4E]">
+                <AdminNavBar />
+              </div>
+
+              <div>
+                <AdminSubNavBar />
+              </div>
+
+              <div className="flex-grow flex items-start justify-center px-4 py-15 z-6 relative">
+                <TablaAdminHistorial />
               </div>
             </div>
           }
---
+++ src/components/ui/TablasAdmin/Productos/TablaAdminCatalogo.tsx
@@ -0,0 +1,104 @@
+import { useEffect } from "react";
+import { useAppSelector } from "../../../../hooks/redux";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../../redux/store";
+import axios from "axios";
+import { fetchProducto } from "../../../../redux/slices/productoSlice";
+import { IProducto } from "../../../../types/IProducto";
+
+export const TablaAdminCatalogo = () => {
+  const { activeSubMenu } = useAppSelector((state) => state.menuActivoAdmin);
+  const productos = useSelector((state: RootState) => state.producto.productos);
+  const dispatch = useDispatch();
+
+  // Función para cargar productos
+  const fetchProducts = async () => {
+
+            try {
+                const response = await axios.get("http://localhost:8080/productos");
+                console.log("Productos desde la API:", response.data);
+                dispatch(fetchProducto(response.data));
+            } catch (err) {
+                console.error("Error al obtener productos:", err);
+            }
+        };
+
+  // Effect para cargar productos cuando se activa el menú
+  useEffect(() => {
+    if (activeSubMenu === "Catálogo") {
+      fetchProducts()
+    }
+  }, [activeSubMenu]);
+  return (
+    <div className="overflow-x-auto">
+      <table className="min-w-full bg-white border border-gray-300 rounded-lg shadow-sm">
+      <thead className="bg-gray-100">
+        <tr>
+          <th className="py-3 px-4 text-left font-semibold border-b">
+            <p>ID</p>
+          </th>
+          <th className="py-3 px-4 text-left font-semibold border-b">
+            <p>Nombre del producto</p>
+          </th>
+          <th className="py-3 px-4 text-left font-semibold border-b">
+            <p>Descripción</p>
+          </th>
+          <th className="py-3 px-4 text-left font-semibold border-b">
+            <p>Categoría</p>
+          </th>
+          <th className="py-3 px-4 text-left font-semibold border-b">
+            <p>Imagen</p>
+          </th>
+          <th className="py-3 px-4 text-left font-semibold border-b">
+            <p>Descuento</p>
+          </th>
+          <th className="py-3 px-4 text-left font-semibold border-b">
+            <p>Tipo</p>
+          </th>
+          <th className="py-3 px-4 text-left font-semibold border-b">
+            <p>Sexo</p>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {productos.length === 0 ? (
+          <tr>
+            <td className="py-8 px-4 text-center text-gray-500 border-b">
+              No hay productos para mostrar
+            </td>
+          </tr>
+        ) : (
+          productos.map((producto:IProducto) => (
+            <tr key={producto.id} className="hover:bg-gray-50">
+              <td className="py-2 px-4 border-b font-medium">
+                {producto.id}
+              </td>
+              <td className="py-2 px-4 border-b">
+                {producto.nombre || 'Sin nombre'}
+              </td>
+              <td className="py-2 px-4 border-b">
+                {producto.descripcion}
+              </td>
+              <td className="py-2 px-4 border-b">
+                {producto.categoria?.nombre || "Sin definir"}
+              </td>
+              <td className="py-2 px-4 border-b">
+                {producto.imagen?.altDescripcion || "Sin definir"}
+              </td>
+              <td className="py-2 px-4 border-b">
+               {producto.descuento?.porcentaje || "Sin definir"}
+              </td>
+              <td className="py-2 px-4 border-b">
+               {producto.tipo || "Sin definir"}
+              </td>
+              <td className="py-2 px-4 border-b">
+               {producto.sexoProducto || "Sin definir"}
+              </td>
+            </tr>
+          ))
+        )}
+      </tbody>
+    </table>
+    </div>
+  )
+}
