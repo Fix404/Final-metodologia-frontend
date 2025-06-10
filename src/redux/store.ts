92841bbecb31bfd59af0f1c250e3aec0d611d0d5
Camila <irisarricamila96@gmail.com>
2025-06-10 12:31:31 -0300

    Merge

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,13 +5,9 @@ import activeAdminMenuSlice from './slices/activeMenuAdminSlice'
 import compraReducer from "./slices/CompraSlice"
 import busquedaReducer from './slices/busquedaSlice';
 import productoReducer from './slices/productoSlice';
-<<<<<<< HEAD
 import usuarioReducer from './slices/usuarioSlice'
 import authReducer from './slices/authSlice'
-=======
-import usuarioReducer from './slices/usuarioSlice';
 import ordenesCompraReducer from "./slices/ordenesCompraSlice";
->>>>>>> c283d32c085d2f354b6c1cf6b8421038b7694ee4
 
 
 const store = configureStore({
