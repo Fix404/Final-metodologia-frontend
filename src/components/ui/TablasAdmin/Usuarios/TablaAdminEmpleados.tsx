75b5f14ff713494e39a341f4557e9c2c71a894cf
Maximiliano Niscola <niscolamaximiliano@gmail.com>
2025-06-09 22:32:45 -0300

    botones empleados

--- src/components/ui/TablasAdmin/Usuarios/TablaAdminEmpleados.tsx
+++ src/components/ui/TablasAdmin/Usuarios/TablaAdminEmpleados.tsx
@@ -1,23 +1,85 @@
 import { useEffect, useState } from "react";
 import { useAppSelector } from "../../../../hooks/redux";
-import { Usuario, usuariosService } from "../../../../services/usuarioService";
+import { usuariosService } from "../../../../services/usuarioService";
+import { IUsuario } from "../../../../types/IUsuario";
+import { useDispatch } from "react-redux";
+import { limpiarUsuarioActivo, setUsuarioActivo } from "../../../../redux/slices/usuarioSlice";
+import Swal from "sweetalert2";
+import { IoEyeSharp, IoTrashBinOutline } from "react-icons/io5";
+import { MdEdit } from "react-icons/md";
+import { UsuarioModal } from "../../Modals/AdminModals/UsuarioModal";
 
 export const TablaAdminEmpleados = () => {
   const { activeSubMenu } = useAppSelector((state) => state.menuActivoAdmin);
-  const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+  const [usuarios, setUsuarios] = useState<IUsuario[]>([]);
+  const [openModal, setOpenModal] = useState(false);
+    const [openModalSee, setOpenModalSee] = useState(false);
+    const usuarioActivo=useAppSelector((state) => state.usuario.usuarioActivo);
+    const dispatch=useDispatch()
 
   // Función para cargar usuarios
   const cargarUsuarios = async () => {
     
     try {
       const data = await usuariosService.obtenerUsuarios();
-      const soloAdmins = data.filter((usuario:Usuario) => 
+      const soloEmpleados = data.filter((usuario:IUsuario) => 
         usuario.rol === "ADMIN"
 );
-setUsuarios(soloAdmins);
-      console.log(soloAdmins)
+setUsuarios(soloEmpleados);
+      console.log(soloEmpleados)
     } catch (err) {
       console.error('Error al cargar usuarios:', err);
+    }
+  };
+  const handleOpenModalVer=(usuario:IUsuario) =>{
+      setOpenModalSee(true);
+      dispatch(setUsuarioActivo(usuario))
+      setOpenModal(true);
+    }
+  
+    const handleOpenModalEdit=(usuario:IUsuario) =>{
+      setOpenModalSee(false);
+      dispatch(setUsuarioActivo(usuario))
+      setOpenModal(true);
+    }
+  
+    const handleOpenModalCrear=() => {
+      dispatch(limpiarUsuarioActivo())
+      setOpenModal(true);
+    }
+  
+    const handleCloseModal= () => {
+      dispatch(limpiarUsuarioActivo())
+      setOpenModal(false);
+      cargarUsuarios()
+    }
+    const handleDelete = async (id: number, usuario: IUsuario) => {
+    const resultado = await Swal.fire({
+      title: '¿Estás seguro?',
+      text: 'Esta acción deshabilitará al usuario.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, deshabilitar',
+      cancelButtonText: 'Cancelar'
+    });
+  
+    if (resultado.isConfirmed) {
+      try {
+        const usuarioActualizado: IUsuario = {
+          ...usuario,
+          activo: false
+        };
+  
+        await usuariosService.eliminarUsuario(id, usuarioActualizado);
+  
+        Swal.fire('Deshabilitado', 'El usuario fue deshabilitado exitosamente.', 'success');
+        cargarUsuarios()
+      } catch (error) {
+        Swal.fire('Error', 'Hubo un problema al deshabilitar el usuario.', 'error');
+        console.error("Hubo un error al borrar el usuario", error);
+      }
     }
   };
 
@@ -28,10 +90,17 @@ setUsuarios(soloAdmins);
     }
   }, [activeSubMenu]);
   return (
+    <>
     <div className="overflow-x-auto">
       <div className="text-4xl font-bold justify-self-center py-7">
         <h1>EMPLEADOS</h1>
       </div>
+      <button
+              onClick={() => handleOpenModalCrear()}
+              className="bg-blue-700 hover:bg-blue-500 text-white text-3xl font-semibold py-1 px-2 cursor-pointer rounded shadow-md transition"
+            >
+              <p>Crear Empleado</p>
+            </button>
       <table className="min-w-full bg-white border border-gray-300 rounded-lg shadow-sm">
       <thead className="bg-gray-100">
         <tr>
@@ -42,16 +111,19 @@ setUsuarios(soloAdmins);
             <p>Nombre</p>
           </th>
           <th className="py-3 px-4 text-left font-semibold border-b">
+            <p>Apellido</p>
+          </th>
+          <th className="py-3 px-4 text-left font-semibold border-b">
             <p>Email</p>
           </th>
           <th className="py-3 px-4 text-left font-semibold border-b">
             <p>Dirección</p>
           </th>
           <th className="py-3 px-4 text-left font-semibold border-b">
-            <p>Estado</p>
+            <p>Rol</p>
           </th>
           <th className="py-3 px-4 text-left font-semibold border-b">
-            <p>Rol</p>
+            <p>Acciones</p>
           </th>
         </tr>
       </thead>
@@ -72,16 +144,43 @@ setUsuarios(soloAdmins);
                 {usuario.nombre || 'Sin nombre'}
               </td>
               <td className="py-2 px-4 border-b">
+                {usuario.apellido || 'Sin apellido'}
+              </td>
+              <td className="py-2 px-4 border-b">
                 {usuario.email}
               </td>
-              <td className="py-2 px-4 border-b">
-                {usuario.direccion || 'No especificada'}
-              </td>
-              <td className="py-2 px-4 border-b">
-                {/* {mostrarEstado(usuario.enabled, usuario.accountNonLocked)} */}
-              </td>
-              <td className="py-2 px-4 border-b">
-                {/* {mostrarRol(usuario.rol)} */}
+                              <td className="py-2 px-4 border-b">
+                                 {typeof usuario.direccion === "object" && usuario.direccion !== null
+                  ? usuario.direccion.calle
+                  : usuario.direccion || "No especificada"}
+                              </td>
+                              <td className="py-2 px-4 border-b">
+                                {usuario.rol}
+                              </td>
+                              <td className="py-2 px-4 border-b">
+                <div className="flex justify-center gap-2">
+                  <button
+                  title="Ver"
+                    onClick={() => handleOpenModalVer(usuario)}
+                    className="bg-green-500 hover:bg-green-400 text-white cursor-pointer w-auto font-semibold py-2 px-2 rounded shadow-md transition"
+                  >
+                    <IoEyeSharp />
+                  </button>
+                  <button
+                    title="Editar"
+                    onClick={() => handleOpenModalEdit(usuario)}
+                    className="bg-purple-500 hover:bg-purple-400 text-white cursor-pointer w-auto font-semibold py-2 px-2 rounded shadow-md transition"
+                  >
+                    <MdEdit />
+                  </button>
+                  <button
+                    title="Eliminar"
+                    onClick={() => handleDelete(usuario.id!, usuario)}
+                    className="bg-red-500 hover:bg-red-400 text-white cursor-pointer w-auto font-semibold py-2 px-2 rounded shadow-md transition"
+                  >
+                    <IoTrashBinOutline />
+                  </button>
+                </div>
               </td>
             </tr>
           ))
@@ -89,5 +188,7 @@ setUsuarios(soloAdmins);
       </tbody>
     </table>
     </div>
+    {openModal && <UsuarioModal activeUser={usuarioActivo} openModalSee={openModalSee} handleCloseModal={handleCloseModal}/>}
+    </>
   )
 }
