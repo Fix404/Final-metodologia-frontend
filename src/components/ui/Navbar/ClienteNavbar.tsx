6e7743939c5f1d6518b777592e63d7d677a8ffa5
Camila <irisarricamila96@gmail.com>
2025-06-11 19:54:14 -0300

    Editar usuario

--- src/components/ui/Navbar/ClienteNavbar.tsx
+++ src/components/ui/Navbar/ClienteNavbar.tsx
@@ -229,7 +229,9 @@ const ClienteNavbar = () => {
 
                     {/* Menu Items */}
                     <div className="py-2">
-                      <button className="w-full px-4 py-3 text-left flex items-center space-x-3 hover:bg-gray-50 transition-colors duration-150 group">
+                      <button 
+                       onClick={() => navigate("/datos-usuario")}
+                      className="w-full px-4 py-3 text-left flex items-center space-x-3 hover:bg-gray-50 transition-colors duration-150 group">
                         <div className="w-6 h-6 bg-blue-100 rounded-md flex items-center justify-center group-hover:bg-blue-200 transition-colors">
                           <svg className="w-3.5 h-3.5 text-blue-900" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
