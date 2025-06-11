ba167627b777c0dc6761b6c8d791ab7c59fb11ee
SophieRF <sofiaferraro2110@gmail.com>
2025-06-11 09:21:47 -0300

    un par de arreglos estéticos

--- src/components/screen/DetalleScreen.tsx
+++ src/components/screen/DetalleScreen.tsx
@@ -22,7 +22,6 @@ const fetchDetallesByProductoId = async (productoId: string): Promise<IDetalle[]
   }
 };
 
-
 const DetalleScreen: React.FC = () => {
   const { id } = useParams<{ id: string }>();
 
@@ -84,7 +83,6 @@ const DetalleScreen: React.FC = () => {
     );
   };
 
-
   const getTallesDisponibles = (): ITalle[] => {
     if (!Array.isArray(detalles) || detalles.length === 0) return [];
     return Array.from(
@@ -102,13 +100,11 @@ const DetalleScreen: React.FC = () => {
     return colorMap[colorName] || '#CCCCCC';
   };
 
-
   const calculateFinalPrice = (detalle: IDetalle): number => {
     const base = detalle.precio?.precioVenta || 0;
     const descuento = detalle.producto?.descuento?.porcentaje ?? 0;
     return Math.round(base * (1 - descuento / 100));
   };
-
 
   const producto = detalles.length > 0 ? detalles[0]?.producto : null;
   const detalleSeleccionado = Array.isArray(detalles) ? detalles.find(
@@ -125,7 +121,6 @@ const DetalleScreen: React.FC = () => {
     dispatch(agregarAlCarrito(detalleSeleccionado));
     dispatch(descontarStock(detalleSeleccionado.id));
   };
-
 
   if (loading) {
     return (
@@ -166,123 +161,135 @@ const DetalleScreen: React.FC = () => {
 
   return (
     <div className="bg-[#fcfcd3] min-h-screen py-8 px-4 flex flex-col items-center justify-start">
-      <div className="max-w-5xl w-full flex flex-col lg:flex-row gap-10 h-full">
-        <div className="w-full lg:w-1/2">
-          <img
-            src={producto?.imagen?.url}
-            alt={producto?.imagen?.altDescripcion}
-            className="max-h-screen h-auto w-auto mx-auto"
-          />
-        </div>
-
-        <div className="pl-5 w-full lg:w-1/2 flex flex-col justify-between flex-1 max-h-screen overflow-auto">
-          <div className="space-y-4">
-            <h1 className="text-3xl font-bold text-gray-800">{producto?.nombre.toUpperCase()}</h1>
-            <p className="text-lg text-gray-600">{producto?.descripcion}</p>
-
-            {/* Colores */}
-            <div>
-              <h3 className="text-lg font-semibold text-gray-700">Color</h3>
-              <div className="flex gap-2 mt-1">
-                {getColoresDisponibles().map((color, index) => (
-                  <button
-                    key={index}
-                    onClick={() => setSelectedColor(color.color)}
-                    style={{ backgroundColor: getColorHex(color.color) }}
-                    className={`w-12 h-12 rounded-full border-4 transition-all ${selectedColor === color.color
-                      ? 'border-gray-600 scale-110 hover:cursor-pointer'
-                      : 'border-gray-300 hover:border-gray-400 hover:cursor-pointer'
-                      }`}
-                    title={color.color}
-                  />
-                ))}
-              </div>
-            </div>
-
-            {/* Talles */}
-            <div>
-              <h3 className="text-lg font-semibold text-gray-700">Talle</h3>
-              <div className="flex gap-3 mt-1">
-                {getTallesDisponibles().map((talle, index) => (
-                  <button
-                    key={index}
-                    onClick={() => setSelectedTalle(talle.talle)}
-                    className={`h-11 w-11 px-3 py-1 rounded-md text-1x2 font-semibold transition-all ${selectedTalle === talle.talle
-                      ? 'bg-[#1c4577] hover:cursor-pointer text-white'
-                      : 'bg-gray-300 hover:bg-gray-400 hover:cursor-pointer text-gray-700'
-                      }`}
-                  >
-                    {talle.talle}
-                  </button>
-                ))}
-              </div>
+      <div className="max-w-6xl w-full">
+        {/* Contenedor principal */}
+        <div className="flex flex-col lg:flex-row gap-8 h-[400px] lg:h-[500px]">
+          
+          {/* Imagen */}
+          <div className="w-full lg:w-1/2 h-full">
+            <div className="w-full h-full rounded-lg shadow-sm overflow-hidden">
+              <img
+                src={producto?.imagen?.url}
+                alt={producto?.imagen?.altDescripcion}
+                className="w-full h-full object-contain"
+              />
             </div>
           </div>
 
-          {/* Parte inferior: precio, stock, botones */}
-          <div className="space-y-4 mt-6">
-
-            <div className="flex justify-between items-center">
-              
-              {/* Stock */}
-              <div className=' flex flex-col'>
-                <span className="font-semibold text-xl text-gray-700">
-                  En carrito: {cantidadEnCarrito}
-                </span>
-                <span className="font-semibold text-gray-700">
-                  Stock disponible: {detalleSeleccionado?.stock != null
-                    ? detalleSeleccionado.stock - cantidadEnCarrito
-                    : 0}
-                </span>
-
+          {/*Info */}
+          <div className="w-full lg:w-1/2 h-full flex flex-col justify-between pl-0 lg:pl-5">
+            
+            {/* Info del producto */}
+            <div className="space-y-6 flex-1">
+              <div>
+                <h1 className="text-3xl font-bold text-gray-800">{producto?.nombre.toUpperCase()}</h1>
+                <p className="text-lg text-gray-600 mt-2">{producto?.descripcion}</p>
               </div>
 
-              {/* Precio */}
-              {detalleSeleccionado && detalleSeleccionado.stock! > 0 && (
-                <div className="flex items-center gap-4">
-                  {producto?.descuento?.porcentaje ? (
-                    <div className="flex items-center gap-2">
-                      <span className="text-gray-500 line-through font-semibold text-xl">
-                        ${detalleSeleccionado.precio?.precioVenta}
-                      </span>
-                      <span className="text-green-600 text-xl font-semibold">
-                        -{producto.descuento.porcentaje}%
-                      </span>
-                      <span className="text-black font-bold text-3xl">
-                        ${calculateFinalPrice(detalleSeleccionado)}
-                      </span>
-                    </div>
-                  ) : (
-                    <span className="text-gray-900 font-bold text-2xl">
-                      ${detalleSeleccionado.precio?.precioVenta}
-                    </span>
-                  )}
+              {/* Colores */}
+              <div>
+                <h3 className="text-lg font-semibold text-gray-700 mb-3">Color</h3>
+                <div className="flex gap-2">
+                  {getColoresDisponibles().map((color, index) => (
+                    <button
+                      key={index}
+                      onClick={() => setSelectedColor(color.color)}
+                      style={{ backgroundColor: getColorHex(color.color) }}
+                      className={`w-12 h-12 rounded-full border-4 transition-all ${
+                        selectedColor === color.color
+                          ? 'border-gray-600 scale-110 hover:cursor-pointer'
+                          : 'border-gray-300 hover:border-gray-400 hover:cursor-pointer'
+                      }`}
+                      title={color.color}
+                    />
+                  ))}
                 </div>
-              )}
+              </div>
+
+              {/* Talles */}
+              <div>
+                <h3 className="text-lg font-semibold text-gray-700 mb-3">Talle</h3>
+                <div className="flex gap-3">
+                  {getTallesDisponibles().map((talle, index) => (
+                    <button
+                      key={index}
+                      onClick={() => setSelectedTalle(talle.talle)}
+                      className={`h-11 w-11 px-3 py-1 rounded-md text-lg font-semibold transition-all ${
+                        selectedTalle === talle.talle
+                          ? 'bg-[#1c4577] hover:cursor-pointer text-white'
+                          : 'bg-gray-300 hover:bg-gray-400 hover:cursor-pointer text-gray-700'
+                      }`}
+                    >
+                      {talle.talle}
+                    </button>
+                  ))}
+                </div>
+              </div>
             </div>
 
-            {/* Botón agregar al carrito */}
-            <button
-              onClick={handleAgregarAlCarrito}
-              disabled={
-                !detalleSeleccionado ||
-                detalleSeleccionado.stock === 0 ||
-                cantidadEnCarrito >= detalleSeleccionado.stock!
-              }
-              className={`w-full flex items-center justify-center gap-3 text-white py-3 px-4 rounded-md transition-all disabled:opacity-50 disabled:cursor-not-allowed hover:cursor-pointer ${detalleSeleccionado && detalleSeleccionado.stock! > 0
-                ? 'bg-[#1c4577] hover:bg-blue-900'
-                : 'bg-gray-400 cursor-not-allowed'
+            {/* Información de compra */}
+            <div className="space-y-4 mt-6">
+              <div className="flex justify-between items-center">
+                
+                {/* Stock */}
+                <div className="flex flex-col">
+                  <span className="font-semibold text-xl text-gray-700">
+                    En carrito: {cantidadEnCarrito}
+                  </span>
+                  <span className="font-semibold text-gray-700">
+                    Stock disponible: {detalleSeleccionado?.stock != null
+                      ? detalleSeleccionado.stock - cantidadEnCarrito
+                      : 0}
+                  </span>
+                </div>
+
+                {/* Precio */}
+                {detalleSeleccionado && detalleSeleccionado.stock! > 0 && (
+                  <div className="flex items-center gap-4">
+                    {producto?.descuento?.porcentaje ? (
+                      <div className="flex items-center gap-2">
+                        <span className="text-gray-500 line-through font-semibold text-xl">
+                          ${detalleSeleccionado.precio?.precioVenta}
+                        </span>
+                        <span className="text-green-600 text-xl font-semibold">
+                          -{producto.descuento.porcentaje}%
+                        </span>
+                        <span className="text-black font-bold text-3xl">
+                          ${calculateFinalPrice(detalleSeleccionado)}
+                        </span>
+                      </div>
+                    ) : (
+                      <span className="text-gray-900 font-bold text-2xl">
+                        ${detalleSeleccionado.precio?.precioVenta}
+                      </span>
+                    )}
+                  </div>
+                )}
+              </div>
+
+              {/* Botón agregar al carrito */}
+              <button
+                onClick={handleAgregarAlCarrito}
+                disabled={
+                  !detalleSeleccionado ||
+                  detalleSeleccionado.stock === 0 ||
+                  cantidadEnCarrito >= detalleSeleccionado.stock!
+                }
+                className={`w-full flex items-center justify-center gap-3 text-white py-4 px-4 rounded-md transition-all disabled:opacity-50 disabled:cursor-not-allowed hover:cursor-pointer ${
+                  detalleSeleccionado && detalleSeleccionado.stock! > 0
+                    ? 'bg-[#1c4577] hover:bg-blue-900'
+                    : 'bg-gray-400 cursor-not-allowed'
                 }`}
-            >
-              <FaCartPlus size={18} />
-              Agregar al carrito
-            </button>
+              >
+                <FaCartPlus size={18} />
+                Agregar al carrito
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
 };
-
 
 export default DetalleScreen;
